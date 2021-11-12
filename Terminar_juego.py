02317da2072f774d85f1a9392f79de126663c18f
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-12 20:40:19 +0100

    Introducimos las variables minimo y maximo, asociadas a MIN y MAX

--- Terminar_juego.py
+++ Terminar_juego.py
@@ -3,17 +3,19 @@ MIN = 0
 MAX = 99
 numero_bueno = random.randint(MIN,MAX)
 
-def pedir_numero ():
+def pedir_numero (minimo=MIN, maximo=MAX):
     while True:
-        entrada = int (input ("Introduce un numero entre " + str(MIN) + " y " + str(MAX) + " :"))
-        if MIN <= entrada <= MAX:
+        entrada = int (input ("Introduce un numero entre " + str(minimo) + " y " + str(maximo) + " :"))
+        if  minimo <= entrada <= maximo:
             break
     return entrada
 
+minimo = MIN
+maximo = MAX
 print ("Comienza el juego, adivina el numero aleatorio")
 
 while True: 
-    intento = pedir_numero()
+    intento = pedir_numero(minimo, maximo)
     if intento < numero_bueno:
         print("Demasiado pequeño")
     elif intento > numero_bueno:
