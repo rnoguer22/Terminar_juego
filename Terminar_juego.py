4ce518cbc81b9dab0026cacffdc61e982c99d9bd
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-12 20:01:22 +0100

    Definimos la funcion pedir_numero

--- Terminar_juego.py
+++ Terminar_juego.py
@@ -1,2 +1,14 @@
 MIN = 0
 MAX = 99
+
+def pedir_numero ():
+    while True:
+        entrada = input ("Introduce un numero entre ", MIN, " y ", MAX, " :")
+        try:
+            int (entrada)
+        except:
+            pass
+        else: 
+            if 0 <= entrada <= 99:
+                break
+        return entrada
