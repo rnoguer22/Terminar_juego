5f6a31690e3a2d2bef337935a17c6787e25cfa64
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-13 13:07:07 +0100

    Introducimos un numero maximo de intentos Establecemos que el limite de intentos sea diferente para cada dificultad

--- Terminar_juego.py
+++ Terminar_juego.py
@@ -1,21 +1,26 @@
 import random
 MIN = 0
 MAX = 0
+intentos_max = 0
 
 while True:
     nivel = input ("Seleccione un nivel, simple, intermedio, avanzado, o experto: ")
     try:
         if nivel == "simple":
             MAX = 100
+            intentos_max = 10
             break
         elif nivel == "intermedio":
             MAX = 1000
+            intentos_max = 20
             break
         elif nivel == "avanzado":
             MAX = 1000000
+            intentos_max = 50
             break
         elif nivel == "experto":
             MAX = 1000000000000
+            intentos_max = 100
             break
     except:
         pass
@@ -25,13 +30,13 @@ def pedir_numero (minimo=MIN, maximo=MAX):
         entrada = input ("Introduce un numero: ")
         try:
             entrada = int (entrada)
-            if  minimo <= entrada <= maximo:
-                break
-            else:
-                print ("Ha introducido un numero que no esta dentro del rango de numeros del juego,"
-                "repita por favor")
         except:
             pass
+        if  minimo <= entrada <= maximo:
+            break
+        else:
+            print ("Ha introducido un numero que no esta dentro del rango de numeros del juego,"
+                "repita por favor")
     return entrada
 
 def ayuda():
@@ -47,10 +52,11 @@ minimo = MIN
 maximo = MAX
 hola = ayuda()
 print ("Comienza el juego, adivina el numero aleatorio")
+print ("Tienes " + str(intentos_max) + " intentos, dale a tope!!!")
 
 contador = 1
 
-while True: 
+while contador < intentos_max: 
     intento = pedir_numero(minimo, maximo)
     if intento < numero_bueno:
         contador += 1
@@ -63,3 +69,6 @@ while True:
         contador += 1
         print ("Ha necesitado ", contador, " intentos para conseguirlo")
         break
+
+if contador == intentos_max:
+    print ("Has alcanzado el numero maximo de intentos, has perdido, paquete...")
