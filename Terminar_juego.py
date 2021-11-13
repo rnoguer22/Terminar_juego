2e929682b0acca36cf5ba7b90816769df9a11f66
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-13 10:01:38 +0100

    Añadimos que el usuario pueda elegir la dificultad Mediante un bucle while

--- Terminar_juego.py
+++ Terminar_juego.py
@@ -1,15 +1,33 @@
 import random
 MIN = 0
-MAX = 99
-numero_bueno = random.randint(MIN,MAX)
+MAX = 0
+
+while True:
+    nivel = input ("Seleccione un nivel, simple, intermedio, avanzado, o experto: ")
+    try:
+        if nivel == "simple":
+            MAX = 100
+            break
+        elif nivel == "intermedio":
+            MAX = 1000
+            break
+        elif nivel == "avanzado":
+            MAX = 1000000
+            break
+        elif nivel == "experto":
+            MAX = 1000000000000
+            break
+    except:
+        pass
 
 def pedir_numero (minimo=MIN, maximo=MAX):
     while True:
-        entrada = int (input ("Introduce un numero entre " + str(minimo) + " y " + str(maximo) + " :"))
+        entrada = int (input ("Introduce un numero entre " + str(minimo) + " y " + str(maximo) + " : "))
         if  minimo <= entrada <= maximo:
             break
     return entrada
 
+numero_bueno = random.randint(MIN,MAX)
 minimo = MIN
 maximo = MAX
 print ("Comienza el juego, adivina el numero aleatorio")
